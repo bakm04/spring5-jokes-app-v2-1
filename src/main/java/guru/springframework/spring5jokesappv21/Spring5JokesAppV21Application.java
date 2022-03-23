d70221b9f5b653d76b1da24ba9fa535783c7d624
bakm04 <mike.baker@csgi.com>
2022-03-22 20:54:52 -0500

    Check in initial project.  Closes #7.

---
+++ src/main/java/guru/springframework/spring5jokesappv21/Spring5JokesAppV21Application.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring5jokesappv21;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5JokesAppV21Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5JokesAppV21Application.class, args);
+    }
+
+}
---
+++ src/test/java/guru/springframework/spring5jokesappv21/Spring5JokesAppV21ApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring5jokesappv21;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5JokesAppV21ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
